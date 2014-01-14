4bbd8108d53c50ee68c95c8be460e83e85d10596
applenele <nele0716@163.com>
2014-01-14 14:30:06 +0800

    第一次提交
    建立项目，完成基本的页面设计，登陆工作完成

---
+++ HOME/Lib/Action/LoginAction.class.php
@@ -0,0 +1,34 @@
+<?php
+    class LoginAction extends Action{
+		
+	    /*实现用户登陆*/
+		public function do_login(){
+			 $username=$_POST['username'];
+             $pwd=$_POST['pwd'];
+			 $user=new Model("User_logn");
+
+			$condition['username']=$username;
+			$condition['password']=$pwd;
+			
+			$user_arr=$user->where($condition)->select();
+			$count = count($user_arr);
+			
+			if($count>0){
+				$_SESSION['username']='nele';
+				$this->success('登录成功');
+                
+			}
+			else{
+				$this->error('用户或密码错误，请重新登录');
+				}
+		}
+		
+		/*注销用户操作*/
+		public function  do_logout(){
+			unset($_SESSION['username']);
+			$this->success('注销成功');
+			}
+		
+		
+	}
+?>
